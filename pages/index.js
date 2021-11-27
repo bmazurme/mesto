2d6323be6cc710ac16f721b325afc0684e33774d
Bogdan <b@bmazur.ru>
2021-11-27 20:36:53 +0300

    #revision2

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,43 @@
-let editButton = document.querySelector('.profile-info__edit');
+let editButton = document.querySelector('.profile__edit');
 let closeButton = document.querySelector('.popup__close');
 let saveButton = document.querySelector('.form__save');
 let form = document.querySelector('.form');
 let popup = document.querySelector('.popup');
-let profileName = document.querySelector('.profile-info__name');
-let profileProfession = document.querySelector('.profile-info__profession');
-let formName = form.querySelector('.form__name');
-let formProfession = form.querySelector('.form__profession');
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
+let formName = form.querySelector('.form__input_type_name');
+let formProfession = form.querySelector('.form__input_type_profession');
 let cover = document.querySelector('.popup__container');
 
-popup.addEventListener('click', overlayClick);
-editButton.addEventListener('click', openForm);
-closeButton.addEventListener('click', closeForm);
-saveButton.addEventListener('click', saveForm);
-popup.addEventListener('keyup', (event)=>{if (event.keyCode === 13) {saveForm()}});
-
 function openForm() {
-  formName.value = profileName.innerHTML;
-  formProfession.value = profileProfession.innerHTML;
+  formName.value = profileName.textContent;
+  formProfession.value = profileProfession.textContent;
   popup.classList.add('popup_active');
-}
-
-function saveForm() {
-  profileName.innerHTML = formName.value;
-  profileProfession.innerHTML = formProfession.value;
-  popup.classList.remove('popup_active');
 }
 
 function closeForm() {
   popup.classList.remove('popup_active');
 }
 
-function overlayClick(e) {
-  if(!cover.contains(e.target)) {
-    closeForm();
-  }
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = formName.value;
+  profileProfession.textContent = formProfession.value;
+  popup.classList.remove('popup_active');
 }
 
+form.addEventListener('submit', formSubmitHandler);
+editButton.addEventListener('click', openForm);
+closeButton.addEventListener('click', closeForm);
+popup.addEventListener('keyup', (event)=>{if (event.keyCode === 13) {saveForm()}});
 
+
+//popup.addEventListener('click', overlayClick);
+// function overlayClick(e) {
+//   if(!cover.contains(e.target)) {
+//     closeForm();
+//   }
+// }
 // let likeButtons = document.getElementsByClassName('element__like');
 // for(var i = 0; i < likeButtons.length; i++) {
 //   likeButtons[i].addEventListener("click", likeChange);
