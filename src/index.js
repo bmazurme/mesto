24467c3237d81d0d66d9c349b5459076b08d3b4e
Bogdan <b@bmazur.ru>
2022-02-01 16:06:08 +0300

    Squashed commit of the following:
    commit bbae42ced25b6cd9b9684123482fa1675776219d
    Author: Bogdan <b@bmazur.ru>
    Date:   Tue Feb 1 16:04:19 2022 +0300
    
        update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,71 @@
-import '../pages/index.css';
+import './pages/index.css'; // добавьте импорт главного файла стилей 
+import { initialCards } from './js/initialCards.js';
+import { config } from './js/config.js';
+import { Card } from './js/components/Card.js';
+import { FormValidator } from './js/components/FormValidator.js';
+import { Section } from './js/components/Section.js';
+import { PopupWithForm } from './js/components/PopupWithForm.js';
+import { UserInfo } from './js/components/UserInfo.js';
 
+const addButton = document.querySelector('.profile__add');
+const editButton = document.querySelector('.profile__edit');
+const formUserProfile = document.querySelector('.form_type_edit');
+const formAddCard = document.querySelector('.form_type_add');
+const saveButton = formAddCard.querySelector('.form__save');
+const nameformUserProfile = formUserProfile.querySelector('.form__input_type_name');
+const professionformUserProfile = formUserProfile.querySelector('.form__input_type_profession');
+const cardListSelector = '.elements';
+const userFormValidator = new FormValidator(config, formUserProfile);
+const cardFormValidator = new FormValidator(config, formAddCard);
+const userInfo = new UserInfo({name: '.profile__name', profession: '.profile__profession'});
 
-const numbers = [2, 3, 5];
+userFormValidator.enableValidation();
+cardFormValidator.enableValidation();
 
-// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2);
+const saveUser = (evt, val) => {
+  evt.preventDefault();
+  const {name, profession} = val;
+  const obj = {name: name.value, profession: profession.value};
+  userInfo.setUserInfo(obj);
+}
 
-console.log(doubledNumbers); // 4, 6, 10
+const saveCard = (evt, val) => {
+  evt.preventDefault();
+  const {name, link} = val;
+  const obj = {name: name.value, link: link.value};
+  const card = new Card(obj, '#card-template');
+  const item = card.createCard();
+  defaultCardList.addItem(item);
+}
+
+const cardPopupWithForm = new PopupWithForm(saveCard, '.popup_type_add');
+const userPopupWithForm = new PopupWithForm(saveUser, '.popup_type_edit');
+
+function openEditCardPopup() {
+  userFormValidator.resetValidation();
+  const data = userInfo.getUserInfo();
+  nameformUserProfile.value = data.name;
+  professionformUserProfile.value = data.profession;
+  userPopupWithForm.open();
+}
+
+function openAddCardPopup() {
+  cardFormValidator.resetValidation();
+  saveButton.classList.add('form__save_inactive');
+  cardPopupWithForm.open();
+}
+
+const defaultCardList = new Section({
+  items: initialCards,
+  renderer: (item) => {
+      const card = new Card(item, '#card-template');
+      const cardElement = card.createCard();
+      defaultCardList.addItem(cardElement);
+    }
+  },
+  cardListSelector
+);
+
+defaultCardList.render();
+editButton.addEventListener('click', openEditCardPopup);
+addButton.addEventListener('click', openAddCardPopup);
