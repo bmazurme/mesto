b1e509c6c815f33ed0a7857c43e8fd20f8cef53b
Bogdan <b@bmazur.ru>
2022-02-03 14:48:54 +0300

    ref config index components

--- src/js/components/PopupWithImage.js
+++ src/js/components/PopupWithImage.js
@@ -1,16 +1,19 @@
-import { Popup } from './Popup.js'; 
+import { Popup } from './Popup.js';
+import { settings } from '../config.js';
 
 export class PopupWithImage extends Popup {
   constructor (card, popupSelector) {
     super(popupSelector);
     this._card = card;
     this._popup = document.querySelector(popupSelector);
+    this._slideImage = settings.slideImage;
+    this._slideName = settings.slideName;
   }
 
   open() {
     super.open();
-    this._image = this._popup.querySelector('.slide__image');
-    this._popup.querySelector('.slide__name').textContent = this._card.name;
+    this._image = this._popup.querySelector(this._slideImage);
+    this._popup.querySelector(this._slideName).textContent = this._card.name;
     this._image.src = this._card.link;
     this._image.alt = this._card.name;
   }
